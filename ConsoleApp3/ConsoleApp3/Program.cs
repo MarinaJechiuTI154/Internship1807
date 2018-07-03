96c32fd0bad2363e6eae96bace5409b9dd9925f2
Jechiu Marina <marinajq95@gmail.com>
2018-07-03 17:45:15 +0300

    add prod function

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 120;
+            b = 150;
+            Console.WriteLine(sum(a, b));
+            Console.WriteLine(prod(a, b));
+
+            Console.ReadKey();
+        }
+
+        public static int sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static int prod(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+
+}
