3aaebec5584e43707bc74678476635843f7e00df
Jechiu Marina <marinajq95@gmail.com>
2018-07-04 11:45:42 +0300

    modified program

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -9,16 +9,12 @@ namespace ConsoleApp3
             int a, b, c;
             a = 5;
             b = 7;
-            Console.WriteLine(sum(a, b));
             Console.WriteLine(exp(a, b));
 
             Console.ReadKey();
         }
 
-        public static int sum(int a, int b)
-        {
-            return a + b;
-        }
+
         public static double exp(int a, int b)
         {
             return Math.Pow(a, b);
